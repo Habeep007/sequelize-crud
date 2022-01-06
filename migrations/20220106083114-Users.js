20ef1ea98ada1f3f4bd8e33b6b6d92a60dca704b
Habeep007 <96163361+Habeep007@users.noreply.github.com>
2022-01-06 14:37:36 +0500

    user-migration
    migration for creating a user table with some attributes

---
+++ migrations/20220106083114-Users.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
